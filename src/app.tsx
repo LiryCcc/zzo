5f324598254dc7c8515720ed289d4def6b4d0948
Liry <learainccc@outlook.com>
2025-07-07 00:06:17 +0800

    chore: add strict mode

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import classNames from 'classnames';
+import { StrictMode } from 'react';
 import s from './app.module.less';
 
 const App = () => {
-  return <div className={classNames(s['app-name'])}>{'Zzo app by Dawanbuchila & Liry'}</div>;
+  return (
+    <StrictMode>
+      <div className={classNames(s['app-name'])}>{'Zzo app by Dawanbuchila & Liry'}</div>
+    </StrictMode>
+  );
 };
 
 export default App;
