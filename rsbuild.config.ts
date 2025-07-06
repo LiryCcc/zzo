73e5060a2650959d6e221946c6341847718b78ed
Liry <learainccc@outlook.com>
2025-07-07 00:05:15 +0800

    ci: config env and deploy

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from '@rsbuild/core';
+import { defineConfig, loadEnv } from '@rsbuild/core';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginReact } from '@rsbuild/plugin-react';
+
+const assetPrefix = process.env.LIRY_ASSET_PREFIX;
+const envPrefixList = ['LIRY_'];
 
 const rsbuildConfig = defineConfig({
   plugins: [pluginReact(), pluginLess()],
   server: {
     port: 29763
+  },
+  source: {
+    define: loadEnv({ prefixes: envPrefixList }).publicVars
+  },
+  output: {
+    assetPrefix
   }
 });
 
