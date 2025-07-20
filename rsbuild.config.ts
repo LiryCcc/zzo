4e577c6127ddc4311b1e14d7608ce6bb8ee7065c
Liry <learainccc@outlook.com>
2025-07-20 14:09:58 +0800

    ci: rsbuild add path alias

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig, loadEnv } from '@rsbuild/core';
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { resolve } from 'node:path';
 
 const assetPrefix = process.env.LIRY_ASSET_PREFIX;
 const envPrefixList = ['LIRY_'];
 
 const rsbuildConfig = defineConfig({
   plugins: [pluginReact(), pluginLess()],
+  resolve: {
+    alias: {
+      '@': resolve(import.meta.dirname, 'src'),
+      '@@': resolve(import.meta.dirname)
+    }
+  },
   server: {
     port: 29763
   },
